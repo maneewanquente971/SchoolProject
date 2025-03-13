d8ca5cd366e0fff791c11d571a15874ae1ba7b86
maneewanquente971 <maneewanquente971@gmail.com>
2025-03-13 12:29:03 +0000

    Add 9.c

---
+++ src/9.c
@@ -0,0 +1,9 @@
+// Simple calculator program that takes two numbers as input and returns their sum
+#include <stdio.h>
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    printf("The sum of the two numbers is: %d\n", num1 + num2);
+    return 0;
+}
