8a7547f00e4114e48d1df0ec71480331ef997dcf
maneewanquente971 <maneewanquente971@gmail.com>
2025-03-14 18:00:35 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int age = 21;
+    if (age >= 18) {
+        printf("You can vote.");
+    } else {
+        printf("Sorry, you are not eligible to vote yet.");
+    }
+}
