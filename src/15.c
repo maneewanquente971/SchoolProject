69554ee306bfccf117cdaba111b073cef3ae4fc3
maneewanquente971 <maneewanquente971@gmail.com>
2025-03-19 13:27:08 +0000

    Add 15.c

---
+++ src/15.c
@@ -0,0 +1,11 @@
+
+int main(void) {
+    int a = 5;
+    int b = 10;
+    if (a > b) {
+        printf("a is greater than b");
+    } else {
+        printf("a is not greater than b");
+    }
+    return 0;
+}
